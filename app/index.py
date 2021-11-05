61be3371328ae9cd599ac49c3c2a519c18a50ac4
Katelyn Chang <alyssum752@gmail.com>
2021-11-04 21:30:50 -0400

    add new queries for product, start changing website layout, add product search bar

--- app/index.py
+++ app/index.py
@@ -1,12 +1,21 @@
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_login import current_user
 import datetime
+
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 from .models.product import Product
 from .models.purchase import Purchase
 
 from flask import Blueprint
 bp = Blueprint('index', __name__)
+
+
+#class SearchForm(Form):
+#  search = StringField('search', [DataRequired()])
+#  submit = SubmitField('Search',
+#                       render_kw={'class': 'btn btn-success btn-block'})
 
 
 @bp.route('/')
@@ -23,3 +32,16 @@ def index():
     return render_template('index.html',
                            avail_products=products,
                            purchase_history=purchases)
+
+@bp.route('/search', methods=['GET', 'POST'])
+def search():
+    name = request.args.get("item")
+    if name == "":
+        matches = Product.get_all(True)
+    else:
+        matches = Product.get_item(name)
+
+    return render_template('index.html',
+                           avail_products=matches,
+                           purchase_history=None)
+
--- app/models/product.py
+++ app/models/product.py
@@ -2,8 +2,9 @@ from flask import current_app as app
 
 
 class Product:
-    def __init__(self, id, name, price, available):
+    def __init__(self, id, category, name, price, available):
         self.id = id
+        self.category = category
         self.name = name
         self.price = price
         self.available = available
@@ -11,7 +12,7 @@ class Product:
     @staticmethod
     def get(id):
         rows = app.db.execute('''
-SELECT id, name, price, available
+SELECT product_id, category, name, price, available
 FROM Products
 WHERE id = :id
 ''',
@@ -21,9 +22,29 @@ WHERE id = :id
     @staticmethod
     def get_all(available=True):
         rows = app.db.execute('''
-SELECT id, name, price, available
+SELECT product_id, category, name, price, available
 FROM Products
 WHERE available = :available
 ''',
                               available=available)
+        return [Product(*row) for row in rows]
+
+    @staticmethod
+    def get_item(name):
+        rows = app.db.execute('''
+SELECT product_id, category, name, price, available
+FROM Products
+WHERE name = :name
+''',
+                              name=name)
+        return [Product(*row) for row in rows]
+
+    @staticmethod
+    def get_category(name):
+        rows = app.db.execute('''
+SELECT product_id, category, name, price, available
+FROM Products
+WHERE name = :category
+''',
+                              name=name)
         return [Product(*row) for row in rows]
