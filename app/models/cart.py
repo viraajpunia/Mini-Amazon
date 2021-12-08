b9caec11d004231bd7636a958ca390e8cf665675
Nathan Kim <njk24@duke.edu>
2021-12-07 20:49:56 -0500

    a bunch of stuff

--- app/models/cart.py
+++ app/models/cart.py
@@ -4,44 +4,30 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from .. import login
 
-#class UserCart:
-#    def __init__(self, uid, product_id, quantity, balance, category, name, descrip, img_link, price):
-#        self.uid = uid
-#        self.product_id = product_id
-#        self.quantity = quantity
-#        self.balance = balance
-#        self.category = category
-#        self.name = name
-#        self.descrip = descrip
-#        self.img_link = img_link
-#        self.price = price
-
-
-#    @staticmethod
-#    def get(uid):
-#        rows = app.db.execute("""
-#SELECT thing1.uid, thing1.product_id, thing1.quantity, thing1.balance, Products.category, Products.name, Products.descrip, Products.img_link, Products.price FROM 
-#    (SELECT UserCarts.uid, UserCarts.product_id, UserCarts.quantity, UserAcc.balance FROM UserCarts LEFT OUTER JOIN UserAcc ON UserCarts.uid = UserAcc.uid) 
-#AS thing1 LEFT OUTER JOIN Products ON thing1.product_id = Products.product_id
-#""",
-#                              uid=uid)
-#        return [UserCart(*row) for row in rows]
-
 class UserCart:
-    def __init__(self, uid, product_id, seller_id, quantity):
+    def __init__(self, uid, product_id, quantity, balance, category, name, descrip, img_link, price):
         self.uid = uid
         self.product_id = product_id
-        self.seller_id = seller_id
         self.quantity = quantity
+        self.balance = balance
+        self.category = category
+        self.name = name
+        self.descrip = descrip
+        self.img_link = img_link
+        self.price = price
+
 
     @staticmethod
     def get(uid):
         rows = app.db.execute("""
-        SELECT uid, product_id, seller_id, quantity
-        FROM UserCarts
-        WHERE uid = :uid
-        """, uid = uid)
+SELECT UserCarts.uid, UserCarts.product_id, UserCarts.quantity, UserInfo.balance, Products.category, Products.name, Products.descrip, Products.img_link, Products.price
+FROM UserCarts, Products, UserInfo
+WHERE UserCarts.uid = UserInfo.uid 
+AND UserCarts.uid = :uid
+AND UserCarts.product_id = Products.product_id
+""",
+                              uid=uid)
         return [UserCart(*row) for row in rows]
-        #return UserCart(*(rows[0])) if rows else None
+
 
 
