1d77c1e636d184b9b9b9cde4743b3f079cc4d7b1
Charles Zhang <cwz3@duke.edu>
2021-12-07 21:11:06 -0500

    display seller's products and ratings

--- app/models/avgratings.py
+++ app/models/avgratings.py
@@ -22,3 +22,15 @@ SELECT CAST(AVG(rating) AS Decimal(10,2))
 FROM Feedback
 ''')
         return AvgRating(*(rows[0])) if rows is not None else None
+
+    @staticmethod
+    def get_item_avg_rating_by_seller(product_id,seller_id):
+        rows = app.db.execute('''
+SELECT CAST(AVG(rating) AS Decimal(10,2))
+FROM Feedback
+WHERE product_id = :product_id
+AND seller_id =:seller_id
+''',
+                              product_id=product_id,
+                              seller_id = seller_id)
+        return AvgRating(*(rows[0])) if rows is not None else None
